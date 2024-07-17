69b899735e1ab18a810c02fc8f8687af96d8588b
JonathanBY15 <bariscanyaprak@gmail.com>
2024-07-17 11:24:27 -0400

    Removed Auth

--- backend_project/api/urls.py
+++ backend_project/api/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('notes', views.note_list_create, name='note-list-create'),
     path('notes/<int:pk>', views.note_detail, name='note-detail'),
-    path('register', views.register, name='register'),
-    path('login', views.user_login, name='login'),
-    path('logout', views.user_logout, name='logout'),
+    # path('register', views.register_view, name='register'),
+    # path('login', views.login_view, name='login'),
+    # path('logout', views.logout_view, name='logout'),
 ]
